8d2e3cd6a656ee1f95a49e65e44a2668d4e132f3
Ajay Lather <ven2aj@sg.uob.com>
2019-12-15 12:42:27 +0800

    First commit

---
+++ AirportLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirportLocator
+//
+//  Created by ven2aj on 15/12/19.
+//  Copyright © 2019 ven2aj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
